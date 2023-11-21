66891518bbe6d62aeb7f5a9583eac1139a7fd55f
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2023-11-20 23:18:45 -0300

    Base Physics and tilemaps

--- game.py
+++ game.py
@@ -1,28 +1,46 @@
 import pygame, sys
+from scripts.utils import load_image, load_images
 from scripts.entities import PhisicsEntity
+from scripts.tilemap import Tilemap
 from pygame.locals import *
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Ninja Game")
+        pygame.display.set_caption('Ninja Game')
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320,240))
+
+
         self.clock = pygame.time.Clock()
 
-        #self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        #self.img = pygame.image.load('data/images/clouds/cloud_1.png')
         #self.img_pos = [160, 260]
         #self.img.set_colorkey((0, 0, 0))
 
-        #self.movement = [False, False]
+        self.movement = [False, False]
+        self.assets = {
+            'decor':load_images('tiles/decor'),
+            'grass':load_images('tiles/grass'),
+            'stone':load_images('tiles/stone'),
+            'large_decor':load_images('tiles/large_decor'),
+            'player': load_image('entities/player.png')
+        }
 
         #self.collision_area = pygame.Rect(50, 50, 300, 50)
 
-        self.player = PhisicsEntity(self,'player',(50,50), (8,15))
+        self.player = PhisicsEntity(self,'player',(50,49), (8,15))
+
+        self.tilemap = Tilemap(self, tile_size=16)
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
+            self.tilemap.render(self.display)
+            self.player.update(self.tilemap, (self.movement[1]-self.movement[0],0))
+            self.player.render(self.display)
+
 
             #collision rectangle
             #img_r = pygame.Rect (self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
@@ -40,22 +58,25 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == KEYDOWN:
-                    if event.key == K_UP or event.key == K_w:
+                    if event.key == K_LEFT or event.key == K_a:
                         self.movement[0] = True
-                    if event.key == K_DOWN or event.key == K_s:
+                    if event.key == K_RIGHT or event.key == K_d:
                         self.movement[1] = True
+                    if event.key== K_UP:
+                        self.player. velocity[1] = -3
                     if event.key == K_ESCAPE:
                         pygame.quit()
                         sys.exit()
                 if event.type == KEYUP:
-                    if event.key == K_UP or event.key == K_w:
+                    if event.key == K_LEFT or event.key == K_a:
                         self.movement[0] = False
-                    if event.key == K_DOWN or event.key == K_s:
+                    if event.key == K_RIGHT or event.key == K_d:
                         self.movement[1] = False
                     if event.key == K_ESCAPE:
                         pygame.quit()
                         sys.exit()
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -4,15 +4,46 @@ class PhisicsEntity:
     def __init__(self, game, e_type, pos, size):
         self.game = game
         self.type = e_type
-        self.pos = list(pos) 
+        self.pos = list(pos)
         self.size = size
-        self.velocity = [0,0]
+        self.velocity = [0, 0]
+        self.colllisions = {'up': False, 'down': False, 'right': False, 'left': False}
 
-    def update (self, movement =(0,0)):
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        self.colllisions = {'up': False, 'down': False, 'right': False, 'left': False}
 
         self.pos[0] += frame_movement[0]
-        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right=rect.left
+                    self.colllisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left=rect.right
+                    self.colllisions['left'] = True
+                self.pos[0] = entity_rect.x
 
-    def render(self,surf):
-        surf.blit(self.game.assets['player'], self.pos)
+        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom=rect.top
+                    self.colllisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top=rect.bottom
+                    self.colllisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        if self.colllisions['down'] or self.colllisions['up']:
+            self.velocity[1]=0
+
+    def render(self, surf):
+        surf.blit(self.game.assets["player"], self.pos)
