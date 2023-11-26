d0111a489c3bb59c67419bebe548d88bcf53c6c7
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2023-11-25 23:35:10 -0300

    Level Editor: Save Map File

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, json
 
 NEIGHBOR_OFFSET = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
 PHYSICS_TILES = {'grass', 'stone'}
@@ -10,10 +10,13 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
-            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
-            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
+        #Añade tejas por defecto, una horizontal de pasto y una segunda vertical de rocas
+        #for i in range(10):
+        #    self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+        #    self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
+    #Detecta las tejas alrededor del pj y añade en un listado aquellas a las que les corresponda crearles físicas.
     def tiles_around(self, pos):
         tiles = []
         tile_loc= (int(pos[0]//self.tile_size), int(pos[1]//self.tile_size))
@@ -23,6 +26,21 @@ class Tilemap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
     
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
+
+    def load(self, path):
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
+    #les crea un rectangulo de colisión a las tejas que se encuentren en el listado creado en la función superior
     def physics_rects_around(self, pos):
         rects = []
         for tile in self.tiles_around(pos):
@@ -30,6 +48,7 @@ class Tilemap:
                 rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
 
+    #renderiza en pantalla el mapa de tejas
     def render(self, surf, offset = (0, 0)):
         for tile in self.offgrid_tiles:
             surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
